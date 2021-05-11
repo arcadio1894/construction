d76cf8b9c7bbb67cde5cfd04fde6d0de17143232
Arcadio1894 <joryes17_1894@hotmail.com>
2021-05-11 10:09:53 -0500

    Ultimos cambios

---
+++ database/seeds/CustomerSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Customer;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Customer::create([
+            'business_name' => 'Danper S.A.C'
+        ]);
+    }
+}
