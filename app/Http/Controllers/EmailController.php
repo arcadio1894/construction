879a4986f9c3f2772bf09cadae176a4f9d08d6ff
Milly <elig.1614@gmail.com>
2021-06-30 23:04:20 -0500

    cambios de front

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -23,6 +23,7 @@ class EmailController extends Controller
                 $email = $request->get('email');
                 $subject = $request->get('subject');
                 $content = $request->get('message');
+                $subject = "Nuevo Mensaje: ".$subject;
                 Mail::to('elig.1614@gmail.com')->send( new ContactMail($name, $email, $subject, $content) );
 
                 DB::commit();
