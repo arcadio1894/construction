237d12a3541c040d9a832f6eb0777f9220e35ab0
Arcadio1894 <joryes17_1894@hotmail.com>
2022-07-04 12:56:16 -0500

    Agregamos el nombre de la cotizacion en el pdf

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -739,7 +739,7 @@ class QuoteController extends Controller
 
         $pdf = PDF::loadHTML($view);
 
-        $name = $quote->code . '.pdf';
+        $name = $quote->code . ' '. $quote->description_quote . '.pdf';
 
         return $pdf->stream($name);
     }
