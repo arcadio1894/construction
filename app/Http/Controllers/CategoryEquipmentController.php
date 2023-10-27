c02e4cb566a472e8e1ed4f20afd78f671924a253
arcadio1894 <joryes1894@gmail.com>
2023-10-26 21:24:34 -0500

    Avance de Pre Cotizaciones

---
+++ app/Http/Controllers/CategoryEquipmentController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CategoryEquipment;
+use Illuminate\Http\Request;
+
+class CategoryEquipmentController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(CategoryEquipment $categoryEquipment)
+    {
+        //
+    }
+
+    public function edit(CategoryEquipment $categoryEquipment)
+    {
+        //
+    }
+
+    public function update(Request $request, CategoryEquipment $categoryEquipment)
+    {
+        //
+    }
+
+    public function destroy(CategoryEquipment $categoryEquipment)
+    {
+        //
+    }
+}
