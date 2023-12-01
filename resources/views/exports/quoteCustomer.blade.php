aa4332c34c70717d10bde7975e93d74d0b6b320f
arcadio1894 <joryes1894@gmail.com>
2023-12-01 17:26:09 -0500

    Cambio en el igv de cotizaciones en soles

--- resources/views/exports/quoteCustomer.blade.php
+++ resources/views/exports/quoteCustomer.blade.php
@@ -229,9 +229,9 @@
         @foreach( $quote->equipments as $equipment )
         <tr>
             <td class="desc">{{ $equipment->description }}</td>
-            <td class="unit">{{ $quote->currency_invoice }} {{ number_format( ((float)($equipment->subtotal_rent/1.18) / $equipment->quantity), 0) }}</td>
+            <td class="unit">{{ $quote->currency_invoice }} {{ ($quote->currency_invoice == 'PEN') ? number_format( ((float)($equipment->subtotal_rent) / $equipment->quantity), 0) : number_format( ((float)($equipment->subtotal_rent/1.18) / $equipment->quantity), 0) }}</td>
             <td class="qty">{{ $equipment->quantity }}</td>
-            <td class="total">{{ $quote->currency_invoice }} {{ number_format( (float)($equipment->subtotal_rent/1.18), 0) }}</td>
+            <td class="total">{{ $quote->currency_invoice }} {{ ($quote->currency_invoice == 'PEN') ? number_format( (float)($equipment->subtotal_rent), 0) : number_format( (float)($equipment->subtotal_rent/1.18), 0) }}</td>
         </tr>
         @endforeach
         </tbody>
@@ -243,7 +243,7 @@
             <td class=""></td>
             <td class=""></td>
             <td class="qty">TOTAL</td>
-            <td class="total">{{ $quote->currency_invoice }} {{ number_format( (float)($quote->total_quote/1.18), 0) }}.00</td>
+            <td class="total">{{ $quote->currency_invoice }} {{ ($quote->currency_invoice == 'PEN') ? number_format( (float)($quote->total_quote), 0) ? : number_format( (float)($quote->total_quote/1.18), 0) }}.00</td>
         </tr>
         {{--<tr>
             <td class="desc"></td>
